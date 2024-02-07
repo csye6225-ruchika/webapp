5672ea4c731c23bd728f4808894e5a12f3204817
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2024-02-07 15:34:14 -0500

    updated env

--- databases/PostgresDBConnection.js
+++ databases/PostgresDBConnection.js
@@ -14,8 +14,8 @@ export const postgresDBConnection = new Sequelize(
     pool: {
       max: parseInt(process.env.DATABASE_POOL_MAX, 10) || 5,
       min: parseInt(process.env.DATABASE_POOL_MIN, 10) || 0,
-      acquire: parseInt(process.env.DATABASE_POOL_MAX, 30000) || 30000,
-      idle: parseInt(process.env.DATABASE_POOL_MAX, 10) || 10000,
+      acquire: parseInt(process.env.DATABASE_POOL_ACQUIRE, 10) || 30000,
+      idle: parseInt(process.env.DATABASE_POOL_IDLE, 10) || 10000,
     },
   }
 );
